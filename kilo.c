967a746705651df7df51bdfe61b0d8e5b6fe6174
Kyle Coffey <mariorules24@gmail.com>
2021-12-07 10:23:22 -0600

    virtual kb input

--- kilo.c
+++ kilo.c
@@ -88,6 +88,37 @@ char EditorReadKey()
 			Die("read");
 		}
 	}
+
+	if (c == '\x1b')
+	{
+		char seq[3];
+
+		if (read(STDIN_FILENO, &seq[0], 1) != 1)
+		{
+			return '\x1b';
+		}
+		if (read(STDIN_FILENO, &seq[1], 1) != 1)
+		{
+			return '\x1b';
+		}
+
+		if (seq[0] == '[')
+		{
+			switch (seq[1])
+			{
+			case 'A':
+				return 'w';
+			case 'B':
+				return 's';
+			case 'C':
+				return 'd';
+			case 'D':
+				return 'a';
+			}
+		}
+
+		return '\x1b';
+	}
 	return c;
 }
 
