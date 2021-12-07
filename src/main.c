ca1a11af7f4e29a3e8bc47b2b97122291c27d0b7
Kyle Coffey <mariorules24@gmail.com>
2021-12-07 09:31:45 -0600

    raw mode

--- src/main.c
+++ src/main.c
@@ -1,7 +1,30 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
+
+struct termios orig_termios;
+
+void enableRawMode()
+{
+	tcgetattr(STDIN_FILENO, &orig_termios);
+
+	struct termios raw = orig_termios;
+
+	raw.c_lflag &= ~(ECHO | ICANON);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+void disableRawMode()
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 int main()
 {
+	enableRawMode();
+	atexit(disableRawMode);
+
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
 	{
