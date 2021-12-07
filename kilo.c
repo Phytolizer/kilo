224905f73eb729500daee354a51cccc55385ee87
Kyle Coffey <mariorules24@gmail.com>
2021-12-07 10:33:53 -0600

    detect DEL

--- kilo.c
+++ kilo.c
@@ -24,6 +24,7 @@ enum EditorKey
 	ARROW_RIGHT,
 	ARROW_UP,
 	ARROW_DOWN,
+	DEL_KEY,
 	HOME_KEY,
 	END_KEY,
 	PAGE_UP,
@@ -129,6 +130,8 @@ int EditorReadKey()
 					{
 					case '1':
 						return HOME_KEY;
+					case '3':
+						return DEL_KEY;
 					case '4':
 						return END_KEY;
 					case '5':
